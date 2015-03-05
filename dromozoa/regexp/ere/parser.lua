aa15ea41fd52cb7d487944b295e67141462e9c33
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-03-05 15:55:01 +0900

    ファイル構造を変更。

--- dromozoa/regexp/ere/parser.lua
+++ dromozoa/regexp/ere/parser.lua
@@ -1,4 +1,19 @@
-local json = require "dromozoa.json"
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-regexp.
+--
+-- dromozoa-regexp is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-regexp is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
 
 local character_class = {
   alnum = true;
@@ -15,7 +30,7 @@ local character_class = {
   xdigit = true;
 }
 
-local function parser()
+return function ()
   local self = {}
 
   function self:parse(text)
@@ -234,16 +249,4 @@ local function parser()
   end
 
   return self
-end
-
-function parse(text)
-  local p = parser()
-  return p:parse(text)
-end
-
-local a, b = parse(arg[1])
-if a then
-  print(json.encode(a))
-else
-  print(b)
 end
