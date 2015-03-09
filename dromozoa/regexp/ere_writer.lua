88f4caef1e6476295d36bc04b62e8ca9e0f7f055
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-03-09 22:59:59 +0900

    ファイル名を変更。

--- dromozoa/regexp/ere_writer.lua
+++ dromozoa/regexp/ere_writer.lua
@@ -24,14 +24,10 @@ return function (out)
     self._out:write(...)
   end
 
-  function self:unparse(node)
-    self:extended_reg_exp(node)
-  end
-
   function self:extended_reg_exp(node)
     self:ERE_branch(node[1])
     for i = 2, #node do
-      self:write "|"
+      self:write("|")
       self:ERE_branch(node[i])
     end
   end
@@ -43,32 +39,31 @@ return function (out)
   end
 
   function self:ERE_expression(node)
-    local t = type(node)
-    if t == "table" then
+    local nodetype = type(node)
+    if nodetype == "table" then
       local a, b = node[1], node[2]
       self:one_char_or_coll_elem_ERE_or_grouping(a)
       if b then
         self:ERE_dupl_symbol(b)
       end
-    elseif t == "string" then
+    elseif nodetype == "string" then
       self:write(node)
     end
   end
 
   function self:one_char_or_coll_elem_ERE_or_grouping(node)
-    local t = type(node)
-    if t == "string" then
+    local nodetype = type(node)
+    if nodetype == "string" then
       if node:match "^[%^%.%[%$%(%)%|%*%+%?%{%\\]$" then
-        self:write "\\"
+        self:write("\\")
       end
       self:write(node)
-    elseif t == "number" then
-      self:write "."
-    elseif t == "table" then
-      local u = type(node[1])
-      if u == "boolean" then
+    elseif nodetype == "number" then
+      self:write(".")
+    elseif nodetype == "table" then
+      if type(node[1]) == "boolean" then
         self:bracket_expression(node)
-      elseif u == "table" then
+      else
         self:write("(")
         self:extended_reg_exp(node)
         self:write(")")
@@ -77,44 +72,44 @@ return function (out)
   end
 
   function self:ERE_dupl_symbol(node)
-    local t = type(node)
-    if t == "string" then
+    local nodetype = type(node)
+    if nodetype == "string" then
       self:write(node)
-    elseif t == "number" then
+    elseif nodetype == "number" then
       self:write("{", node, "}")
-    elseif t == "table" then
+    elseif nodetype == "table" then
       local a, b = node[1], node[2]
       self:write("{", a, ",")
       if b then
         self:write(b)
       end
-      self:write "}"
+      self:write("}")
     end
   end
 
   function self:bracket_expression(node)
-    self:write "["
+    self:write("[")
     if not node[1] then
-      self:write "^"
+      self:write("^")
     end
     for i = 2, #node do
       self:expression_term(node[i])
     end
-    self:write "]"
+    self:write("]")
   end
 
   function self:expression_term(node)
-    local t = type(node)
-    if t == "table" then
+    local nodetype = type(node)
+    if nodetype == "table" then
       local a, b = node[1], node[2]
       if b then
         self:end_range(a)
-        self:write "-"
+        self:write("-")
         self:end_range(b)
       else
         self:write("[:", a, ":]")
       end
-    elseif t == "string" then
+    elseif nodetype == "string" then
       self:end_range(node)
     end
   end
