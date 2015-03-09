529ec53e613f458839e3f638376f2afd19a0da03
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-03-10 00:28:22 +0900

    dot_writerを追加。

--- test2.lua
+++ test2.lua
@@ -1,8 +1,10 @@
 local buffer_writer = require "dromozoa.regexp.buffer_writer"
+local ere_parser = require "dromozoa.regexp.ere_parser"
 local ere_unparser = require "dromozoa.regexp.ere_unparser"
+local dot_writer = require "dromozoa.regexp.dot_writer"
+local nfa_builder = require "dromozoa.regexp.nfa_builder"
 
-ere_unparser(io.stdout):extended_reg_exp {
-  { "^", { "a" }, { "b" }, { { false, { "a", "z" }, { "digit" } } } };
-  { "^", { "c", "*" } };
-}
-io.write("\n")
+local p = ere_parser()
+local a = p:parse(arg[1])
+local nfa = nfa_builder():build(a)
+dot_writer(io.stdout):nfa(nfa)
