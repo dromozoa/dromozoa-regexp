9aac08ab6b669b74a67c4ff71eeb1aa696cc8e43
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-03-12 09:57:00 +0900

    anchoringも含めるように修正。

--- test2.lua
+++ test2.lua
@@ -3,13 +3,14 @@ local buffer_writer = require "dromozoa.regexp.buffer_writer"
 local ere_parser = require "dromozoa.regexp.ere_parser"
 local ere_unparser = require "dromozoa.regexp.ere_unparser"
 local dot_writer = require "dromozoa.regexp.dot_writer"
-local nfa_builder = require "dromozoa.regexp.nfa_builder"
+local fsm_builder = require "dromozoa.regexp.fsm_builder"
 local dfa_builder = require "dromozoa.regexp.dfa_builder"
 
 local p = ere_parser()
 local a = p:parse(arg[1])
-local nfa = nfa_builder():build(a)
-dot_writer(io.stdout):nfa(nfa)
+local nfa = fsm_builder():build(a)
+-- print(json.encode(nfa))
+dot_writer(io.stdout):fsm(nfa)
 -- local dfa = dfa_builder():build(nfa)
 -- print(json.encode(dfa))
 -- dot_writer(io.stdout):nfa(dfa)
