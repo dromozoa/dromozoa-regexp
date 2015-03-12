7ee174adb4f95c1a1a35a7eb292dc509dd8e6d60
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-03-13 08:17:45 +0900

    fsmにwrite_dotを追加。

--- dromozoa/regexp/fsm.lua
+++ dromozoa/regexp/fsm.lua
@@ -15,7 +15,32 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
 
+local buffer_writer = require "dromozoa.regexp.buffer_writer"
+local ere_unparser = require "dromozoa.regexp.ere_unparser"
 local graph = require "dromozoa.regexp.graph"
+
+local graphviz_quote = {
+  ["\""] = "&quot;";
+  ["&"] = "&amp;";
+  ["<"] = "&lt;";
+  [">"] = "&gt;";
+}
+
+local function graphviz_label(c)
+  if type(c) == "number" and c >= 0 then
+    if c == 0 then
+      return "<<font color=\"#CC0000\">&epsilon;</font>>"
+    elseif c == 1 then
+      return "<<font color=\"#CC0000\">^</font>>"
+    elseif c == 2 then
+      return "<<font color=\"#CC0000\">$</font>>"
+    end
+  else
+    local out = buffer_writer()
+    ere_unparser(out):one_char_or_coll_elem_ERE_or_grouping(c)
+    return "<" .. out:concat():gsub("[\"&<>]", graphviz_quote) .. ">"
+  end
+end
 
 return function ()
   local self = {
@@ -46,6 +71,18 @@ return function ()
 
   function self:add_accept(v)
     self._accept[v] = true
+  end
+
+  function self:write_graphviz(out)
+    out:write("digraph \"fsm\" {\n")
+    out:write("  graph [rankdir = LR]\n")
+    for k, v in pairs(self._accept) do
+      out:write("  ", k, " [peripheries = 2];\n")
+    end
+    for i, e in self:each_edge() do
+      out:write("  ", e[1], " -> ", e[2], " [label = ", graphviz_label(e[3]), "];\n")
+    end
+    out:write("}\n")
   end
 
   return self
