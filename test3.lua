d975b0ccbc0217af0b27e0f27e98dd199eb88dfa
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-03-17 15:42:13 +0900

    graphベースに変更。

--- test3.lua
+++ test3.lua
@@ -1,10 +1,12 @@
-local json = require "dromozoa.json"
 local ere_parser = require "dromozoa.regexp.ere_parser"
 local nfa_builder = require "dromozoa.regexp.nfa_builder"
+local write_graphviz = require "dromozoa.regexp.write_graphviz"
 
 local ast = ere_parser():parse(arg[1])
 local nfa = nfa_builder():build(ast)
-local dfa = nfa:build_powerset()
-dfa:remove_assertion()
-dfa:write_graphviz(io.stdout)
+write_graphviz(nfa, io.stdout)
+
+-- local dfa = nfa:build_powerset()
+-- dfa:remove_assertion()
+-- dfa:write_graphviz(io.stdout)
 -- print(json.encode(nfa))
