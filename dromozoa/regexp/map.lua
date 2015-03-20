61909ce5e3ee29f7d42defc673d950ef4a700763
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-03-20 09:14:56 +0900

    mapを追加。

---
+++ dromozoa/regexp/map.lua
@@ -0,0 +1,98 @@
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-regexp.
+--
+-- dromozoa-regexp is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-regexp is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
+
+return function ()
+  local self = {
+    _t = {};
+  }
+
+  function self:find(key)
+    local n = #key
+    local t = self._t
+    local u = t[n]
+    if not u then
+      return nil
+    end
+    for i = 1, n - 1 do
+      u = u[key[i]]
+      if not u then
+        return nil
+      end
+    end
+    return u[key[n]]
+  end
+
+  function self:insert(key, value)
+    local n = #key
+    local t = self._t
+    local u = t[n]
+    if not u then
+      u = {}
+      t[n] = u
+    end
+    for i = 1, n - 1 do
+      local k = key[i]
+      local v = u[k]
+      if not v then
+        v = {}
+        u[k] = v
+      end
+      u = v
+    end
+    local k = key[n]
+    local v = u[k]
+    if v == nil then
+      u[k] = value
+      return value, true
+    else
+      return v, false
+    end
+  end
+
+  function self:erase(key)
+    local n = #key
+    local t = self._t
+    local u = t[n]
+    if not u then
+      return nil
+    end
+    local s = { u }
+    for i = 1, n - 1 do
+      u = u[key[i]]
+      if not u then
+        return nil
+      end
+      s[#s + 1] = u
+    end
+    local k = key[n]
+    local v = u[k]
+    if v == nil then
+      return nil
+    else
+      for i = #s, 1, -1 do
+        local w = s[i]
+        w[k] = nil
+        if next(w) ~= nil then
+          break
+        end
+      end
+      return v
+    end
+  end
+
+  return self
+end
--- test4.lua
+++ test4.lua
@@ -1,9 +1,14 @@
 local json = require "dromozoa.json"
-local decode_condition = require "dromozoa.regexp.decode_condition"
-local encode_condition = require "dromozoa.regexp.encode_condition"
-local unparse_ere = require "dromozoa.regexp.unparse_ere"
+local map = require "dromozoa.regexp.map"
 
-local c = decode_condition { "[^", { "[-", "a", "z" } }
-print(json.encode(encode_condition(c)))
-print(unparse_ere(encode_condition(c)))
+local m = map()
+m:insert({1,2,3}, false)
+m:insert({1,2,3}, "foo")
+m:insert({1,2,2}, "bar")
+m:insert({1,3,4}, "baz")
+m:insert({2,3,4}, "qux")
+print(m:find({1,2,3}))
+print(m:erase({1,2,3}))
+print(m:find({1,2,3}))
+print(json.encode(m._t))
 
