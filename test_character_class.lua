006e63d8f9d3442e9f2628fba74aa43713e6e35e
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-03-24 15:29:55 +0900

    character_classのバグを修正。

---
+++ test_character_class.lua
@@ -0,0 +1,24 @@
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-regexp.
+--
+-- dromozoa-regexp is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-regexp is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
+
+local character_class = require "dromozoa.regexp.character_class"
+
+assert(character_class.xdigit)
+assert(character_class.punct:test(string.byte "["))
+assert(character_class.punct:test(string.byte "|"))
+assert(character_class.punct:test(string.byte "]"))
+assert(character_class.punct:test(string.byte "~"))
