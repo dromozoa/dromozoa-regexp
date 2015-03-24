16ebf061bd610381def1e88a2abe0fee422ee6fa
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-03-24 10:43:51 +0900

    ファイル名を変更。

--- test3.lua
+++ test3.lua
@@ -2,7 +2,7 @@ local json = require "dromozoa.json"
 local write_graphviz = require "dromozoa.regexp.write_graphviz"
 local parse_ere = require "dromozoa.regexp.parse_ere"
 local unparse_ere = require "dromozoa.regexp.unparse_ere"
-local create_nfa = require "dromozoa.regexp.create_nfa"
+local decode = require "dromozoa.regexp.decode"
 local construct_subset = require "dromozoa.regexp.construct_subset"
 local graph = require "dromozoa.graph"
 local minimize = require "dromozoa.regexp.minimize"
@@ -10,7 +10,7 @@ local minimize = require "dromozoa.regexp.minimize"
 local ast = parse_ere(arg[1])
 -- print(json.encode(ast))
 -- print(unparse_ere(ast))
-local nfa = create_nfa(ast)
+local nfa = decode(ast)
 write_graphviz(nfa, io.open("test-nfa.dot", "w")):close()
 local dfa = construct_subset(nfa)
 write_graphviz(dfa, io.open("test-dfa1.dot", "w")):close()
