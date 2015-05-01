8eaf1927baf028bd1c127b20e40594fc5be32ea3
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-05-01 13:32:19 +0900

    リファクタリング。

--- dromozoa/regexp/tree_map.lua
+++ dromozoa/regexp/tree_map.lua
@@ -15,8 +15,22 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
 
---[====[
-local function construct()
+local function each(i, n, data, key)
+  i = i + 1
+  if i < n then
+    for k, v in pairs(data) do
+      key[i] = k
+      each(i, n, v, key)
+    end
+  else
+    for k, v in pairs(data) do
+      key[i] = k
+      coroutine.yield(key, v)
+    end
+  end
+end
+
+return function ()
   local _dataset = {}
 
   local self = {}
@@ -101,110 +115,8 @@ local function construct()
   end
 
   function self:each()
-  end
-
-  return self
-end
-]====]
-
-
-local function each(i, n, data, key)
-  i = i + 1
-  if i < n then
-    for k, v in pairs(data) do
-      key[i] = k
-      each(i, n, v, key)
-    end
-  else
-    for k, v in pairs(data) do
-      key[i] = k
-      coroutine.yield(key, v)
-    end
-  end
-end
-
-return function ()
-  local self = {
-    _t = {};
-  }
-
-  function self:find(key)
-    local n = #key
-    local t = self._t
-    local u = t[n]
-    if not u then
-      return nil
-    end
-    for i = 1, n do
-      u = u[key[i]]
-      if u == nil then
-        return nil
-      end
-    end
-    return u
-  end
-
-  function self:insert(key, value)
-    local n = #key
-    local t = self._t
-    local u = t[n]
-    if not u then
-      u = {}
-      t[n] = u
-    end
-    for i = 1, n - 1 do
-      local k = key[i]
-      local v = u[k]
-      if not v then
-        v = {}
-        u[k] = v
-      end
-      u = v
-    end
-    local k = key[n]
-    local v = u[k]
-    if v == nil then
-      u[k] = value
-      return value, true
-    else
-      return v, false
-    end
-  end
-
-  function self:erase(key)
-    local n = #key
-    local t = self._t
-    local u = t[n]
-    if not u then
-      return nil
-    end
-    local s = { u }
-    for i = 1, n - 1 do
-      u = u[key[i]]
-      if not u then
-        return nil
-      end
-      s[#s + 1] = u
-    end
-    local v = u[key[n]]
-    if v == nil then
-      return nil
-    else
-      for i = #s, 1, -1 do
-        local w = s[i]
-        w[key[i]] = nil
-        if next(w) ~= nil then
-          return v
-        end
-      end
-      t[n] = nil
-      return v
-    end
-  end
-
-  function self:each()
     return coroutine.wrap(function ()
-      for n, data in pairs(self._t) do
+      for n, data in pairs(_dataset) do
         local key = {}
         each(0, n, data, key)
       end
