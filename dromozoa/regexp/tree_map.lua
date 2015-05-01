ba471644e6964d838749e16e6973bd1e908bcd6a
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-05-01 13:29:04 +0900

    リファクタリング。

--- dromozoa/regexp/tree_map.lua
+++ dromozoa/regexp/tree_map.lua
@@ -15,37 +15,111 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
 
-local function each_n(ctx, n)
-  local t = ctx.t
-  n = next(t, n)
-  if n then
-    local key = {}
-    local s = { t[n] }
-    ctx.s = s
-    for i = 1, n do
-      key[i], s[i + 1] = next(s[i])
-    end
-    return key, s[n + 1]
-  end
-end
+--[====[
+local function construct()
+  local _dataset = {}
 
-local function each(ctx, key)
-  if key then
+  local self = {}
+
+  function self:insert(key, value)
     local n = #key
-    local s = ctx.s
-    for i = n, 1, -1 do
-      local k, v = next(s[i], key[i])
-      key[i], s[i + 1] = k, v
-      if k then
-        for j = i + 1, n do
-          key[j], s[j + 1] = next(s[j])
-        end
-        return key, s[n + 1]
-      end
+    local data = _dataset[n]
+    if not data then
+      data = {}
+      _dataset[n] = data
     end
-    return each_n(ctx, n)
+    for i = 1, n - 1 do
+      local k = key[i]
+      local v = data[k]
+      if not v then
+        v = {}
+        data[k] = v
+      end
+      data = v
+    end
+    local k = key[n]
+    local v = data[k]
+    if v == nil then
+      data[k] = value
+      return value, true
+    else
+      return v, false
+    end
+  end
+
+  function self:erase(key)
+    local n = #key
+    local data = _dataset[n]
+    if not data then
+      return
+    end
+    local dataset = { data }
+    for i = 1, n - 1 do
+      local k = key[i]
+      local v = data[k]
+      if not v then
+        return
+      end
+      data = v
+      dataset[#dataset + 1] = v
+    end
+    local k = key[n]
+    local v = data[k]
+    if v == nil then
+      return
+    else
+      for i = #dataset, 1, -1 do
+        local data = dataset[i]
+        local k = key[i]
+        data[k] = nil
+        if next(data) ~= nil then
+          return v
+        end
+      end
+      _dataset[n] = nil
+      return v
+    end
+  end
+
+  function self:find(key)
+    local n = #key
+    local data = _dataset[n]
+    if not data then
+      return
+    end
+    for i = 1, n - 1 do
+      local k = key[i]
+      local v = data[k]
+      if not v then
+        return
+      end
+      data = v
+    end
+    local k = key[n]
+    local v = data[k]
+    return v
+  end
+
+  function self:each()
+  end
+
+  return self
+end
+]====]
+
+
+local function each(i, n, data, key)
+  i = i + 1
+  if i < n then
+    for k, v in pairs(data) do
+      key[i] = k
+      each(i, n, v, key)
+    end
   else
-    return each_n(ctx)
+    for k, v in pairs(data) do
+      key[i] = k
+      coroutine.yield(key, v)
+    end
   end
 end
 
@@ -129,7 +203,12 @@ return function ()
   end
 
   function self:each()
-    return each, { t = self._t }
+    return coroutine.wrap(function ()
+      for n, data in pairs(self._t) do
+        local key = {}
+        each(0, n, data, key)
+      end
+    end)
   end
 
   return self
