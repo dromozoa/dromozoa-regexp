433f5093ae630344cb362f5f4f002f03ff4f1629
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-05-01 16:51:34 +0900

    リファクタリング。

--- dromozoa/regexp/bitset_to_node.lua
+++ dromozoa/regexp/bitset_to_node.lua
@@ -15,7 +15,43 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
 
-local string_char = string.char
+local function queue()
+  local _a = {}
+  local _b = {}
+
+  local self = {}
+
+  function self:push(i)
+    local n = #_b
+    if n > 0 then
+      local b = _b[n]
+      if b == i - 1 then
+        _b[n] = i
+        return
+      end
+    end
+    n = n + 1
+    _a[n] = i
+    _b[n] = i
+  end
+
+  function self:front()
+    return _a[1], _b[1]
+  end
+
+  function self:each()
+    local n = #_b
+    local i = 0
+    return function ()
+      i = i + 1
+      if i <= n then
+        return _a[i], _b[i]
+      end
+    end
+  end
+
+  return self
+end
 
 return function (set)
   local n = set:count()
@@ -29,31 +65,42 @@ return function (set)
     return { "$" }
   else
     local is_matching_list = n < 128
-    local t = {}
-    for i = 0, 255 do
-      if set:test(i) == is_matching_list then
-        local v = t[#t]
-        if v and v[2] == i - 1 then
-          v[2] = i
-        else
-          t[#t + 1] = { i, i }
+
+    local q = queue()
+    if is_matching_list then
+      for i = 0, 255 do
+        if set:test(i) then
+          q:push(i)
+        end
+      end
+    else
+      for i = 0, 255 do
+        if not set:test(i) then
+          q:push(i)
         end
       end
     end
+
     if n == 1 then
-      return { "char", string_char(t[1][1]) }
+      local a = q:each()()
+      return { "char", string.char(a) }
     end
-    local node = { is_matching_list and "[" or "[^" }
-    for i = 1, #t do
-      local v = t[i]
-      local a, b = v[1], v[2]
+
+    local node
+    if is_matching_list then
+      node = { "[" }
+    else
+      node = { "[^" }
+    end
+
+    for a, b in q:each() do
       if a == b then
-        node[#node + 1] = { "[char", string_char(a) }
+        node[#node + 1] = { "[char", string.char(a) }
       elseif a == b - 1 then
-        node[#node + 1] = { "[char", string_char(a) }
-        node[#node + 1] = { "[char", string_char(b) }
+        node[#node + 1] = { "[char", string.char(a) }
+        node[#node + 1] = { "[char", string.char(b) }
       else
-        node[#node + 1] = { "[-", { "[char", string_char(a) }, { "[char", string_char(b) } }
+        node[#node + 1] = { "[-", { "[char", string.char(a) }, { "[char", string.char(b) } }
       end
     end
     return node
