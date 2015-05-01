7bb7fb5cc732870f9aa9a1ff70fa675b1f8381fb
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-05-01 14:32:23 +0900

    リファクタリング。

--- dromozoa/regexp/tree_map.lua
+++ dromozoa/regexp/tree_map.lua
@@ -15,6 +15,51 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
 
+local function find(i, n, data, key, path)
+  i = i + 1
+  local k = key[i]
+  local v = data[k]
+  if path then
+    path[i + 1] = v
+  end
+  if i < n then
+    if v then
+      return find(i, n, v, key, path)
+    end
+  else
+    return v
+  end
+end
+
+local function insert(i, n, data, key, value)
+  i = i + 1
+  local k = key[i]
+  local v = data[k]
+  if i < n then
+    if not v then
+      v = {}
+      data[k] = v
+    end
+    return insert(i, n, v, key, value)
+  else
+    if v == nil then
+      data[k] = value
+      return value, true
+    else
+      return v, false
+    end
+  end
+end
+
+local function erase(i, key, path)
+  local k = key[i]
+  local v = path[i]
+  v[k] = nil
+  if i > 1 and not next(v) then
+    return erase(i - 1, key, path)
+  end
+end
+
 local function each(i, n, data, key)
   i = i + 1
   if i < n then
@@ -42,55 +87,18 @@ return function ()
       data = {}
       _dataset[n] = data
     end
-    for i = 1, n - 1 do
-      local k = key[i]
-      local v = data[k]
-      if not v then
-        v = {}
-        data[k] = v
-      end
-      data = v
-    end
-    local k = key[n]
-    local v = data[k]
-    if v == nil then
-      data[k] = value
-      return value, true
-    else
-      return v, false
-    end
+    return insert(0, n, data, key, value)
   end
 
   function self:erase(key)
     local n = #key
     local data = _dataset[n]
-    if not data then
-      return
-    end
-    local dataset = { data }
-    for i = 1, n - 1 do
-      local k = key[i]
-      local v = data[k]
-      if not v then
-        return
+    if data then
+      local path = { data }
+      local v = find(0, n, data, key, path)
+      if v ~= nil then
+        erase(n, key, path)
       end
-      data = v
-      dataset[#dataset + 1] = v
-    end
-    local k = key[n]
-    local v = data[k]
-    if v == nil then
-      return
-    else
-      for i = #dataset, 1, -1 do
-        local data = dataset[i]
-        local k = key[i]
-        data[k] = nil
-        if next(data) ~= nil then
-          return v
-        end
-      end
-      _dataset[n] = nil
       return v
     end
   end
@@ -98,27 +106,15 @@ return function ()
   function self:find(key)
     local n = #key
     local data = _dataset[n]
-    if not data then
-      return
+    if data then
+      return find(0, n, data, key)
     end
-    for i = 1, n - 1 do
-      local k = key[i]
-      local v = data[k]
-      if not v then
-        return
-      end
-      data = v
-    end
-    local k = key[n]
-    local v = data[k]
-    return v
   end
 
   function self:each()
     return coroutine.wrap(function ()
       for n, data in pairs(_dataset) do
-        local key = {}
-        each(0, n, data, key)
+        each(0, n, data, {})
       end
     end)
   end
