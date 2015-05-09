6b0505537bd6aa8d2838c976b7429cd6fe6cef12
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-05-10 06:28:09 +0900

    ファイル名を変更。

--- test_product.lua
+++ test_product.lua
@@ -16,15 +16,15 @@
 -- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
 
 local construct_nfa = require "dromozoa.regexp.construct_nfa"
-local construct_product = require "dromozoa.regexp.construct_product"
 local minimize = require "dromozoa.regexp.minimize"
 local parse = require "dromozoa.regexp.parse"
 local powerset_construction = require "dromozoa.regexp.powerset_construction"
+local product_construction = require "dromozoa.regexp.product_construction"
 local write_graphviz = require "dromozoa.regexp.write_graphviz"
 
 local dfa1 = minimize(powerset_construction(construct_nfa(parse(arg[1]))))
 local dfa2 = minimize(powerset_construction(construct_nfa(parse(arg[2]))))
-local dfa3 = construct_product[arg[3]](dfa1, dfa2)
+local dfa3 = product_construction[arg[3]](dfa1, dfa2)
 local dfa4 = minimize(dfa3)
 
 write_graphviz(dfa1, assert(io.open("test-dfa1.dot", "w"))):close()
