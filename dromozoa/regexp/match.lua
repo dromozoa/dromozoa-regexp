97acab4000e8102b8a390470360eff6c0d7cc83e
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-05-12 20:20:30 +0900

    matchの高速化。

--- dromozoa/regexp/match.lua
+++ dromozoa/regexp/match.lua
@@ -25,8 +25,8 @@ return function (code, s, i, j)
   if not i then i = 1 end
   if not j then j = #s end
 
-  local cs = code.start
-  local ns
+  local sa = code.start
+  local sb
   local nonaccept_max = code.nonaccept_max
   local accept_tokens = code.accept_tokens
   local transitions = code.transitions
@@ -44,23 +44,39 @@ return function (code, s, i, j)
 ]]
 
   for i = 1, n do
+    local s
+    local n
+    local j = ""
+    if i % 2 == 1 then
+      s = "sa"
+      n = "sb"
+    else
+      s = "sb"
+      n = "sa"
+    end
+    if i == 1 then
+      j = " - 1"
+    elseif i > 2 then
+      j = " + " .. (i - 2)
+    end
+
     buffer = buffer .. string.format([[
     if b%02d then
-      ns = transitions[cs * 257 + b%02d]
+      ]]..n..[[ = transitions[ ]]..s..[[ * 257 + b%02d]
     else
-      ns = transitions[cs * 257 + 256]
+      ]]..n..[[ = transitions[ ]]..s..[[ * 257 + 256]
     end
-    if not ns then
-      if cs > nonaccept_max then
-        return accept_tokens[cs - nonaccept_max], i - 1
+    if not ]]..n..[[ then
+      if ]]..s..[[ > nonaccept_max then
+        return accept_tokens[ ]]..s..[[ - nonaccept_max], i%s
       else
         return
       end
     end
-    i = i + 1
-    cs = ns
-]], i, i)
+]], i, i, j)
   end
+
+  buffer = buffer .. string.format("i = i + %d\n", n)
 
   buffer = buffer .. [[
   end
@@ -70,40 +86,55 @@ end
   return buffer
 end
 
---[====[
+-- print(generate(4))
 return assert(loadstring(generate(32)))()
-]====]
 
+--[====[
 local string_byte = string.byte
 
 return function (code, s, i, j)
   if not i then i = 1 end
   if not j then j = #s end
 
-  local cs = code.start
-  local ns
+  local sa = code.start
+  local sb
   local nonaccept_max = code.nonaccept_max
   local accept_tokens = code.accept_tokens
   local transitions = code.transitions
 
   while true do
-    local k = i
+    local k = i + 1
     if k > j then k = j end
 
-    local b01 = string_byte(s, i, k)
+    local b01, b02 = string_byte(s, i, k)
+
     if b01 then
-      ns = transitions[cs * 257 + b01]
+      sb = transitions[sa * 257 + b01]
     else
-      ns = transitions[cs * 257 + 256]
+      sb = transitions[sa * 257 + 256]
     end
-    if not ns then
-      if cs > nonaccept_max then
-        return accept_tokens[cs - nonaccept_max], i - 1
+    if not sb then
+      if sa > nonaccept_max then
+        return accept_tokens[sa - nonaccept_max], i - 1
       else
         return
       end
     end
-    i = i + 1
-    cs = ns
+
+    if b02 then
+      sa = transitions[sb * 257 + b02]
+    else
+      sa = transitions[sb * 257 + 256]
+    end
+    if not sa then
+      if sb > nonaccept_max then
+        return accept_tokens[sb - nonaccept_max], i
+      else
+        return
+      end
+    end
+
+    i = i + 2
   end
 end
+]====]
