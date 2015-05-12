ebae08fa1bba3ca82c1f1e32c4718ce081f16eae
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-05-13 07:55:06 +0900

    matchの高速化。

--- dromozoa/regexp/match.lua
+++ dromozoa/regexp/match.lua
@@ -18,12 +18,17 @@
 local loadstring = loadstring or load
 
 function generate(n)
+  local vars = "b01"
+  for i = 2, n do
+    vars = vars .. string.format(", b%02d", i)
+  end
+
   local buffer = string.format([[
 local string_byte = string.byte
 
-return function (code, s, i, j)
-  if not i then i = 1 end
-  if not j then j = #s end
+return function (code, s, m, n)
+  if not m then m = 1 end
+  if not n then n = #s end
 
   local sa = code.start
   local sb
@@ -31,17 +36,33 @@ return function (code, s, i, j)
   local accept_tokens = code.accept_tokens
   local transitions = code.transitions
 
-  while true do
-    local k = i + %d
-    if k > j then k = j end
-    local b01]], n - 1)
+  for i = m + %d, n, %d do
+    local %s = string_byte(s, i - %d, i)
+]], n - 1, n, vars, n - 1)
 
-  for i = 2, n do
-    buffer = buffer .. string.format(", b%02d", i)
+  for i = 1, n do
+    local s
+    local n
+    local j = string.format(" - %d", 5 - i)
+    if i % 2 == 1 then
+      s = "sa"
+      n = "sb"
+    else
+      s = "sb"
+      n = "sa"
+    end
+
+    buffer = buffer .. string.format([[
+    ]]..n..[[ = transitions[]]..s..[[ * 257 + b%02d]
+    if not ]]..n..[[ then if ]]..s..[[ > nonaccept_max then return accept_tokens[]]..s..[[ - nonaccept_max], i]]..j..[[ else return end end
+]], i)
   end
-  buffer = buffer .. [[
- = string_byte(s, i, k)
-]]
+
+  buffer = buffer .. string.format([[
+  end
+  local i = n + 1 - (n - m + 1) %% %d
+  local %s = string_byte(s, i, n)
+]], n, vars)
 
   for i = 1, n do
     local s
@@ -57,29 +78,22 @@ return function (code, s, i, j)
     if i == 1 then
       j = " - 1"
     elseif i > 2 then
-      j = " + " .. (i - 2)
+      j = string.format(" + %d", i - 2)
     end
 
     buffer = buffer .. string.format([[
-    if b%02d then
-      ]]..n..[[ = transitions[ ]]..s..[[ * 257 + b%02d]
-    else
-      ]]..n..[[ = transitions[ ]]..s..[[ * 257 + 256]
-    end
-    if not ]]..n..[[ then
-      if ]]..s..[[ > nonaccept_max then
-        return accept_tokens[ ]]..s..[[ - nonaccept_max], i%s
-      else
-        return
-      end
-    end
-]], i, i, j)
+  if b%02d then
+    ]]..n..[[ = transitions[]]..s..[[ * 257 + b%02d]
+  else
+    ]]..n..[[ = transitions[]]..s..[[ * 257 + 256]
   end
-
-  buffer = buffer .. string.format("i = i + %d\n", n)
+  if not ]]..n..[[ then if ]]..s..[[ > nonaccept_max then return accept_tokens[]]..s..[[ - nonaccept_max], i]]..j..[[ else return end end
+]], i, i)
+  end
 
   buffer = buffer .. [[
-  end
+  sb = transitions[sa * 257 + 256]
+  if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i + 3 else return end end
 end
 ]]
 
@@ -87,10 +101,9 @@ end
 end
 
 -- print(generate(4))
---[====[
-return assert(loadstring(generate(32)))()
-]====]
+return assert(loadstring(generate(64)))()
 
+--[====[
 local string_byte = string.byte
 
 return function (code, s, m, n)
@@ -102,10 +115,6 @@ return function (code, s, m, n)
   local nonaccept_max = code.nonaccept_max
   local accept_tokens = code.accept_tokens
   local transitions = code.transitions
-
-  -- local x = n - m + 1
-  -- x = x - x % 4
-  -- x = x + m
 
   -- 1,1  n=1  0 1,0    x=1   4,-3
   -- 1,2  n=2  0 1,0    x=1   4,-3
@@ -122,42 +131,14 @@ return function (code, s, m, n)
 
   for i = m + 3, n, 4 do
     local b01, b02, b03, b04 = string_byte(s, i - 3, i)
-
     sb = transitions[sa * 257 + b01]
-    if not sb then
-      if sa > nonaccept_max then
-        return accept_tokens[sa - nonaccept_max], i - 4
-      else
-        return
-      end
-    end
-
+    if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i - 4 else return end end
     sa = transitions[sb * 257 + b02]
-    if not sa then
-      if sb > nonaccept_max then
-        return accept_tokens[sb - nonaccept_max], i - 3
-      else
-        return
-      end
-    end
-
+    if not sa then if sb > nonaccept_max then return accept_tokens[sb - nonaccept_max], i - 3 else return end end
     sb = transitions[sa * 257 + b03]
-    if not sb then
-      if sa > nonaccept_max then
-        return accept_tokens[sa - nonaccept_max], i - 2
-      else
-        return
-      end
-    end
-
+    if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i - 2 else return end end
     sa = transitions[sb * 257 + b04]
-    if not sa then
-      if sb > nonaccept_max then
-        return accept_tokens[sb - nonaccept_max], i - 1
-      else
-        return
-      end
-    end
+    if not sa then if sb > nonaccept_max then return accept_tokens[sb - nonaccept_max], i - 1 else return end end
   end
 
   local i = n + 1 - (n - m + 1) % 4
@@ -165,90 +146,76 @@ return function (code, s, m, n)
 
   if b01 then
     sb = transitions[sa * 257 + b01]
-    if not sb then
-      if sa > nonaccept_max then
-        return accept_tokens[sa - nonaccept_max], i - 1
-      else
-        return
-      end
-    end
+    if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i - 1 else return end end
   else
     sb = transitions[sa * 257 + 256]
-    if not sb then
-      if sa > nonaccept_max then
-        return accept_tokens[sa - nonaccept_max], i - 1
-      else
-        return
-      end
-    end
+    if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i - 1 else return end end
   end
 
   if b02 then
     sa = transitions[sb * 257 + b02]
-    if not sa then
-      if sb > nonaccept_max then
-        return accept_tokens[sb - nonaccept_max], i
-      else
-        return
-      end
-    end
+    if not sa then if sb > nonaccept_max then return accept_tokens[sb - nonaccept_max], i else return end end
   else
     sa = transitions[sb * 257 + 256]
-    if not sa then
-      if sb > nonaccept_max then
-        return accept_tokens[sb - nonaccept_max], i
-      else
-        return
-      end
-    end
+    if not sa then if sb > nonaccept_max then return accept_tokens[sb - nonaccept_max], i else return end end
   end
 
   if b03 then
     sb = transitions[sa * 257 + b03]
-    if not sb then
-      if sa > nonaccept_max then
-        return accept_tokens[sa - nonaccept_max], i + 1
-      else
-        return
-      end
-    end
+    if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i + 1 else return end end
   else
     sb = transitions[sa * 257 + 256]
-    if not sb then
-      if sa > nonaccept_max then
-        return accept_tokens[sa - nonaccept_max], i + 1
-      else
-        return
-      end
-    end
+    if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i + 1 else return end end
   end
 
   if b04 then
     sa = transitions[sb * 257 + b04]
-    if not sa then
-      if sb > nonaccept_max then
-        return accept_tokens[sb - nonaccept_max], i + 2
-      else
-        return
-      end
-    end
+    if not sa then if sb > nonaccept_max then return accept_tokens[sb - nonaccept_max], i + 2 else return end end
   else
     sa = transitions[sb * 257 + 256]
-    if not sa then
-      if sb > nonaccept_max then
-        return accept_tokens[sb - nonaccept_max], i + 2
-      else
-        return
-      end
-    end
+    if not sa then if sb > nonaccept_max then return accept_tokens[sb - nonaccept_max], i + 2 else return end end
   end
 
   sb = transitions[sa * 257 + 256]
-  if not sb then
-    if sa > nonaccept_max then
-      return accept_tokens[sa - nonaccept_max], i + 3
+  if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i + 3 else return end end
+
+--[[
+  if b02 then
+    sb = transitions[sa * 257 + b01]
+    if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i - 1 else return end end
+    sa = transitions[sb * 257 + b02]
+    if not sa then if sb > nonaccept_max then return accept_tokens[sb - nonaccept_max], i else return end end
+  else
+    if b01 then
+      sb = transitions[sa * 257 + b01]
+      if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i - 1 else return end end
     else
-      return
+      sb = transitions[sa * 257 + 256]
+      if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i - 1 else return end end
     end
+    sa = transitions[sb * 257 + 256]
+    if not sa then if sb > nonaccept_max then return accept_tokens[sb - nonaccept_max], i else return end end
   end
+
+  if b04 then
+    sb = transitions[sa * 257 + b03]
+    if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i + 1 else return end end
+    sa = transitions[sb * 257 + b04]
+    if not sa then if sb > nonaccept_max then return accept_tokens[sb - nonaccept_max], i + 2 else return end end
+  else
+    if b03 then
+      sb = transitions[sa * 257 + b03]
+      if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i + 1 else return end end
+    else
+      sb = transitions[sa * 257 + 256]
+      if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i + 1 else return end end
+    end
+    sa = transitions[sb * 257 + 256]
+    if not sa then if sb > nonaccept_max then return accept_tokens[sb - nonaccept_max], i + 2 else return end end
+  end
+
+  sb = transitions[sa * 257 + 256]
+  if not sb then if sa > nonaccept_max then return accept_tokens[sa - nonaccept_max], i + 3 else return end end
+]]
 end
+]====]
