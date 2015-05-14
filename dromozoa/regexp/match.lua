6ec2e95c9f09a8a445c5cd928c9103bc27726237
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-05-14 15:55:13 +0900

    簡易テンプレートに移行。

--- dromozoa/regexp/match.lua
+++ dromozoa/regexp/match.lua
@@ -15,20 +15,21 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
 
+local buffer_writer = require "dromozoa.regexp.buffer_writer"
+local template = require "dromozoa.regexp.template"
+
 local loadstring = loadstring or load
 
-local function generate_params(format, n)
-  local buffer = {}
-  for i = 1, n do
-    buffer[i] = string.format(format, i)
-  end
-  return table.concat(buffer, ", ")
-end
-
-local function generate(n)
-  local params = generate_params("b%02d", n)
-
-  local buffer = string.gsub([[
+local code = assert(loadstring(template([====[
+[% local function params() %]
+b1[% for i = 2, n do %], b[%= i %][% end %]
+[% end %]
+[% local function cs(i) %]
+[% if i % 2 == 1 then %]sa[% else %]sb[% end %]
+[% end %]
+[% local function ns(i) %]
+[% if i % 2 == 1 then %]sb[% else %]sa[% end %]
+[% end %]
 local string_byte = string.byte
 
 return function (code, s, m, n)
@@ -41,74 +42,37 @@ return function (code, s, m, n)
   local transitions = code.transitions
   local end_assertions = code.end_assertions
 
-  for i = m + {:m:}, n, {:n:} do
-    local {:params:} = string_byte(s, i - {:m:}, i)
-]], "{:(.-):}", {
-    m = n - 1;
-    n = n;
-    params = params;
-  })
-
-  for i = 1, n do
-    buffer = buffer .. string.gsub([[
-    {:ns:} = transitions[{:cs:} * 256 + {:b:}]
-    if not {:ns:} then
-      return accepts[{:cs:}], i - {:m:}
+  for i = m + [%= n - 1 %], n, [%= n %] do
+    local [% params() %] = string_byte(s, i - [%= n - 1 %], i)
+[% for i = 1, n do %]
+    [% ns(i) %] = transitions[[% cs(i) %] * 256 + b[%= i %]]
+    if not [% ns(i) %] then
+      return accepts[[% cs(i) %]], i - [%= n - i + 1 +%]
     end
-]], "{:(.-):}", {
-      cs = i % 2 == 1 and "sa" or "sb";
-      ns = i % 2 == 1 and "sb" or "sa";
-      b = string.format("b%02d", i);
-      m = n - i + 1
-    })
+[% end %]
   end
 
-  buffer = buffer .. string.gsub([[
-  end
-  local i = n + 1 - (n - m + 1) % {:n:}
-  local {:params:} = string_byte(s, i, n)
-]], "{:(.-):}", {
-    n = n;
-    params = params;
-  })
-
-  for i = 1, n do
-    local m = ""
-    if i == 1 then
-      m = " - 1"
-    elseif i > 2 then
-      m = string.format(" + %d", i - 2)
-    end
-
-    buffer = buffer .. string.gsub([[
-  if {:b:} then
-    {:ns:} = transitions[{:cs:} * 256 + {:b:}]
-    if not {:ns:} then
-      return accepts[{:cs:}], i{:m:}
+  local i = n + 1 - (n - m + 1) % [%= n +%]
+  local [% params() %] = string_byte(s, i, n)
+[% for i = 1, n do %]
+  if b[%= i %] then
+    [% ns(i) %] = transitions[[% cs(i) %] * 256 + b[%= i %]]
+    if not [% ns(i) %] then
+      return accepts[[%= cs(i) %]], i[% if i == 1 then %] - 1[% elseif i > 2 then %] + [%= i - 2 %][% end +%]
     end
   else
-    {:ns:} = end_assertions[{:cs:}]
-    if not {:ns:} then
-      return accepts[{:cs:}], i{:m:}
-    end
+    [% ns(i) %] = end_assertions[[% cs(i) %]]
   end
-]], "{:(.-):}", {
-      cs = i % 2 == 1 and "sa" or "sb";
-      ns = i % 2 == 1 and "sb" or "sa";
-      b = string.format("b%02d", i);
-      m = m
-    })
+  if not [% ns(i) %] then
+    return accepts[[%= cs(i) %]], i[% if i == 1 then %] - 1[% elseif i > 2 then %] + [%= i - 2 %][% end +%]
   end
+[% end %]
 
-  buffer = buffer .. string.gsub([[
   sb = transitions[sa]
   if not sb then
-    return accepts[sa], i + {:m:}
+    return accepts[sa], i + [%= n - 1 +%]
   end
 end
-]], "{:(.-):}", { m = n - 1 })
+]====])))()
 
-  return buffer
-end
-
-return assert(loadstring(generate(64)))()
+return assert(loadstring(code({ n = 64 }, buffer_writer()):concat()))()
