2abe5899d3651eb9e9c20228332dab4e5a82fe6b
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-10-05 08:18:43 +0900

    リファクタリング。

--- dromozoa/regexp/bitset.lua
+++ dromozoa/regexp/bitset.lua
@@ -15,61 +15,71 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
 
-return function ()
-  local _data = {}
+local pairs = require "dromozoa.commons.pairs"
 
-  local self = {}
+local class = {}
 
-  function self:set(m, n)
-    if not n then n = m end
-    for i = m, n do
-      _data[i] = true
-    end
-    return self
+function class.new()
+  return {}
+end
+
+function class:set(m, n)
+  if not n then n = m end
+  for i = m, n do
+    self[i] = true
   end
-
-  function self:reset(m, n)
-    if not n then n = m end
-    for i = m, n do
-      _data[i] = nil
-    end
-    return self
-  end
-
-  function self:flip(m, n)
-    if not n then n = m end
-    for i = m, n do
-      if _data[i] then
-        _data[i] = nil
-      else
-        _data[i] = true
-      end
-    end
-    return self
-  end
-
-  function self:set_union(that)
-    for i in that:each() do
-      self:set(i)
-    end
-    return self
-  end
-
-  function self:test(i)
-    return _data[i]
-  end
-
-  function self:each()
-    return next, _data
-  end
-
-  function self:count()
-    local n = 0
-    for _ in pairs(_data) do
-      n = n + 1
-    end
-    return n
-  end
-
   return self
 end
+
+function class:reset(m, n)
+  if not n then n = m end
+  for i = m, n do
+    self[i] = nil
+  end
+  return self
+end
+
+function class:flip(m, n)
+  if not n then n = m end
+  for i = m, n do
+    if self[i] then
+      self[i] = nil
+    else
+      self[i] = true
+    end
+  end
+  return self
+end
+
+function class:set_union(that)
+  for i in that:each() do
+    self:set(i)
+  end
+  return self
+end
+
+function class:test(i)
+  return self[i]
+end
+
+function class:each()
+  return pairs(self)
+end
+
+function class:count()
+  local n = 0
+  for _ in pairs(self) do
+    n = n + 1
+  end
+  return n
+end
+
+local metatable = {
+  __index = class;
+}
+
+return setmetatable(class, {
+  __call = function ()
+    return setmetatable(class.new(), metatable)
+  end;
+})
