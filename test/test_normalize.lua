fff43b88f3ef533a51263128ead98ce3dfe22479
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-11-18 14:25:50 +0900

    テストをLua 5.2に対応。

--- test/test_normalize.lua
+++ test/test_normalize.lua
@@ -15,9 +15,10 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-regexp.  If not, see <http://www.gnu.org/licenses/>.
 
+local ipairs = require "dromozoa.commons.ipairs"
 local regexp = require "dromozoa.regexp"
 
-local function test_normalize(this, that, that2, that3)
+local function test_normalize(this, that)
   local ast = regexp.syntax_tree.ere(this)
   ast:normalize()
   ast:node_to_condition()
@@ -33,24 +34,32 @@ local function test_normalize(this, that, that2, that3)
     dfa = dfa:minimize()
     dfa:write_graphviz(assert(io.open("test4.dot", "w"))):close()
     local result = dfa:to_ast():denormalize():to_ere(true)
-    -- print(result)
-    assert(result == that or result == that2 or result == that3)
+    print(result)
+    local check = false
+    for _, v in ipairs(that) do
+      if result == v then
+        check = true
+        break
+      end
+    end
+    assert(check)
   end
 end
 
-test_normalize("^abc|d^ef|gh$i|jkl$|mno$$|^pqr$|^^stu$$|^$^$|$^$^",
-               "(^j|j)kl$|^($|mno$)|^abc|^pqr$|^stu$|mno$",
-               "^($|jkl$|mno$)|^abc|^pqr$|^stu$|jkl$|mno$")
-test_normalize("(^j|j)kl$|^($|mno$)|^abc|^pqr$|^stu$|mno$",
-               "^($|jkl$|mno$)|^abc|^pqr$|^stu$|jkl$|mno$")
-test_normalize("^($|jkl$|mno$)|^abc|^pqr$|^stu$|jkl$|mno$",
-               "(^j|j)kl$|^($|mno$)|^abc|^pqr$|^stu$|mno$",
-               "^($|jkl$|mno$)|^abc|^pqr$|^stu$|jkl$|mno$",
-               "(^m|m)no$|^($|jkl$)|^abc|^pqr$|^stu$|jkl$")
-test_normalize("(^m|m)no$|^($|jkl$)|^abc|^pqr$|^stu$|jkl$",
-               "^($|jkl$|mno$)|^abc|^pqr$|^stu$|jkl$|mno$")
-test_normalize("^foo", "^foo")
-test_normalize("^foo$", "^foo$")
-test_normalize("foo", "foo")
-test_normalize("foo$", "^foo$|foo$", "(^f|f)oo$")
+local data = {
+  "^abc|d^ef|gh$i|jkl$|mno$$|^pqr$|^^stu$$|^$^$|$^$^";
+  "^($|jkl$|mno$)|^abc|^pqr$|^stu$|jkl$|mno$";
+  "(^j|j)kl$|^($|mno$)|^abc|^pqr$|^stu$|mno$";
+  "(^j|j)kl$|(^m|m)no$|^$|^abc|^pqr$|^stu$";
+  "(^m|m)no$|^($|jkl$)|^abc|^pqr$|^stu$|jkl$";
+}
+test_normalize("^abc|d^ef|gh$i|jkl$|mno$$|^pqr$|^^stu$$|^$^$|$^$^", data)
+for _, v in ipairs(data) do
+  test_normalize(v, data)
+end
+
+test_normalize("^foo", { "^foo" })
+test_normalize("^foo$", { "^foo$" })
+test_normalize("foo", { "foo" })
+test_normalize("foo$", { "^foo$|foo$", "(^f|f)oo$" })
 test_normalize("$foo^")
